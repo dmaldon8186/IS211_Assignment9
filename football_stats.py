c14feeec5915139cd21c03863425e7a2cb875f90
dmaldon8186 <david.maldonado36@spsmail.cuny.edu>
2023-04-03 01:48:11 -0400

    Assignment Complete

--- football_stats.py
+++ football_stats.py
@@ -1,2 +1,17 @@
-if __name__ == "__main__":
-    pass
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.cbssports.com/nfl/stats/player/passing/nfl/regular/qualifiers/?sortcol=td&sortdir=descending'
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+table = soup.find('tbody')
+rows = table.find_all('tr')
+
+for row in rows[:20]:
+    cells = row.find_all('td')
+    if len(cells) > 1:
+        name = cells[0].find('a').text.strip()
+        team = cells[0].find("span", attrs = {"class":"CellPlayerName-team"}).text.strip()
+        touchdowns = cells[8].text.strip()
+        print(name, team, touchdowns)
